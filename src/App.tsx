b8d484ddccc85388f78495becd8e2630d7fb8dca
PhalaUdupa <phalachandra.udupa@labelnest.in>
2026-01-16 15:27:23 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+console.log('🚀 main.tsx is running');
+
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
